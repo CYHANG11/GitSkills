cfeadacdc711b77e289eced2a5dc9b9a706ed1b1
chenyuhang <280523386@qq.com>
2016-12-12 20:42:22 +0800

    addChat

---
+++ ChatViewController.m
@@ -0,0 +1,72 @@
+//
+//  ChatViewController.m
+//  HXXMPP
+//
+//  Created by 陈裕航 on 16/12/9.
+//  Copyright © 2016年 陈裕航. All rights reserved.
+//
+
+#import "ChatViewController.h"
+
+@interface ChatViewController ()<EMChatManagerDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *TextFile;
+
+@property (weak, nonatomic) IBOutlet UILabel *GetMassage;
+
+
+@property (nonatomic,strong) EMConversation *conversation;
+@end
+
+@implementation ChatViewController
+
+- (instancetype)initWithConversationId:(NSString *)conversationId conversationType:(EMConversationType)conversationType
+{
+    self=[super initWithNibName:@"ChatViewController" bundle:nil];
+    if (self) {
+        _conversation=[[EMClient sharedClient].chatManager getConversation:conversationId type:conversationType createIfNotExist:YES];
+        [[EMClient sharedClient].chatManager addDelegate:self delegateQueue:nil];
+    }
+    
+
+    return self;
+
+}
+- (void)dealloc
+{
+    [[EMClient sharedClient].chatManager removeDelegate:self];
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (IBAction)SendMessage:(id)sender {
+    
+    EMTextMessageBody *boby=[[EMTextMessageBody alloc]initWithText:_TextFile.text];
+    EMMessage *message=[[EMMessage alloc]initWithConversationID:_conversation.conversationId from:[EMClient sharedClient].currentUsername to:_conversation.conversationId body:boby ext:nil];
+    
+    [[EMClient sharedClient].chatManager sendMessage:message progress:nil completion:^(EMMessage *message, EMError *error) {
+        if (!error) {
+            NSLog(@"succeed");
+        }
+        
+    }];
+    
+}
+
+-(void)messagesDidReceive:(NSArray *)aMessages
+{
+    NSLog(@"接受消息%ld 条",aMessages.count);
+
+
+}
+
+
+
+
+
+@end
